06ce1289d04da670508a30b4a50a993d9a1711eb
Ino Gamalinda <ino@gamalinda.com>
2025-08-04 03:38:10 +0800

    Add placeholder for image header

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,25 +4,51 @@ import shared
 struct ContentView: View {
     private let contentRepository = ContentRepository()
     @State private var cars: [Car] = []
-
+    
     var body: some View {
-        List(cars, id: \.model) { car in
-            HStack {
-                Rectangle()
-                    .frame(width: 72, height: 64)
-                VStack(alignment: .leading) {
-                    Text("\(car.make) \(car.model)")
-                    Text("Price: \(car.customerPrice)")
-                    Text("Rating: \(car.rating)")
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 0) {
+                    Rectangle()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 256)
+                    VStack(alignment: .leading) {
+                        ForEach(cars, id: \.model) { car in
+                            HStack {
+                                Rectangle()
+                                    .frame(width: 72, height: 64)
+                                VStack(alignment: .leading) {
+                                    Text("\(car.make) \(car.model)")
+                                    Text("Price: \(car.customerPrice)")
+                                    Text("Rating: \(car.rating)")
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(.yellow)
+                        }
+                    }
+                    .background(.blue)
                 }
             }
-        }
-        .onAppear {
-            contentRepository.getCars() { result, error in
-                if let result = result {
-                    cars = result
-                } else if let error = error {
-                    print("Error: \(error)")
+            .background(.red)
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(0)
+            .onAppear {
+                contentRepository.getCars() { result, error in
+                    if let result = result {
+                        cars = result
+                    } else if let error = error {
+                        print("Error: \(error)")
+                    }
+                }
+            }
+            .toolbar{
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Text("GUIDOMIA")
+                        Spacer()
+                        Image(systemName: "line.3.horizontal")
+                    }
                 }
             }
         }
@@ -30,7 +56,7 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }

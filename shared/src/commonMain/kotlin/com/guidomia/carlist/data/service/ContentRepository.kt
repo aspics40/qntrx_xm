bc61a1704e53e2933ee552d90bb2e85e352f5152
Ino Gamalinda <ino@gamalinda.com>
2025-08-04 07:14:18 +0800

    Sanitize car list pros and cons

--- shared/src/commonMain/kotlin/com/guidomia/carlist/data/service/ContentRepository.kt
+++ shared/src/commonMain/kotlin/com/guidomia/carlist/data/service/ContentRepository.kt
@@ -28,6 +28,37 @@ object ContentRepository {
         if (mutableCarList.isEmpty()) {
             fetchCars()
         }
-        return mutableCarList.toList()
+
+        return mutableCarList.map { car ->
+            sanitizeCar(car)
+        }.toList()
+    }
+
+    private fun sanitizeCar(
+        car: Car
+    ) : Car {
+        var consList = car.consList.toMutableList()
+        if (consList.isEmpty()) {
+            consList.add("None")
+        }
+
+        consList = consList.filter { it.isNotBlank() }.toMutableList()
+
+        var prosList = car.prosList.toMutableList()
+        if (prosList.isEmpty()) {
+            prosList.add("None")
+        }
+
+        prosList = prosList.filter { it.isNotBlank()}.toMutableList()
+
+        return Car(
+            consList.toList(),
+            car.customerPrice,
+            car.make,
+            car.marketPrice,
+            car.model,
+            prosList.toList(),
+            car.rating
+        )
     }
 }
